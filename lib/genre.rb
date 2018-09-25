f67367e7c09ba378be775f3964eda5f72eeb91db
Brandon Green <brandon@brandonleeproductions.com>
2018-09-25 00:12:10 +0000

    automatically backed up by learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,37 +1,37 @@
-# class Genre
-#
-#     attr_accessor :genre, :song
-#
-#   @@all = []
-#
-#   def self.all
-#     @@all
-#   end
-#
-#   def new_song(name, artist)
-#     name.genre = self
-#   end
-#
-# end
+class Genre
+
+    attr_accessor :genre, :song
+
+  @@all = []
+
+  def self.all
+    @@all
+  end
+
+  def new_song(name, artist)
+    name.genre = self
+  end
+
+end
 
 
-# class Genre
-#   attr_accessor :name, :songs
-# 
-#   def initialize(name)
-#     @name = name
-#     @songs = []
-#   end
-# 
-#   def add_song(song)
-#     @songs << song
-#   end
-# 
-#   def artists
-#     @songs.all.select do |song|
-#       song.artist == self
-#     end
-#   end
-# 
-# 
-# end
+class Genre
+  attr_accessor :name, :songs
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def add_song(song)
+    @songs << song
+  end
+
+  def artists
+    @songs.all.select do |song|
+      song.artist == self
+    end
+  end
+
+
+end
