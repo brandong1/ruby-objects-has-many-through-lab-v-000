d6f01cf69e623fa62377d0255b7d99f45de6d072
Brandon Green <brandon@brandonleeproductions.com>
2018-09-25 00:20:44 +0000

    automatically backed up by learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,21 +1,4 @@
 class Genre
-
-    attr_accessor :genre, :song
-
-  @@all = []
-
-  def self.all
-    @@all
-  end
-
-  def new_song(name, artist)
-    name.genre = self
-  end
-
-end
-
-
-class Genre
   attr_accessor :name, :songs
 
   def initialize(name)
